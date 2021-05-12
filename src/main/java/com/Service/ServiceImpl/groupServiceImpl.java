36e3851d36a8668761f688533a44694a8a0b265e
captain1fs <69740798+captain1fs@users.noreply.github.com>
2021-05-12 10:41:33 +0800

    分组功能controller
    顺哥看看

--- src/main/java/com/Service/ServiceImpl/groupServiceImpl.java
+++ src/main/java/com/Service/ServiceImpl/groupServiceImpl.java
@@ -15,6 +15,7 @@ public class groupServiceImpl implements groupService{
 
     Select select = new Select();
     @Override
+    //根据学生id将选择course课程的所有学生分为groupNum组
     public void setGroupBySid(Course course, int groupNum){
         System.out.println("将课程所有学生分组");
         Student[] stuList = select.SelectStudent(course);
@@ -28,6 +29,15 @@ public class groupServiceImpl implements groupService{
     public void quickAnswer(int groupId){
         System.out.println("小组抢答");
     }
+
+    //展示当前课程分组结果
+    public Student[] displayGroup(Course course){
+        System.out.println("展示分组");
+        Student[] stuList = select.SelectStudent(course);
+        return stuList;
+    }
+
+    //将当前课程第groupId组的学生平时成绩加上seminarScore
     public void setGroupScoreByGid(Course course,String groupId,String seminarScore){
         System.out.println("给小组成员计分");
         //按照组号查询choose表，将该组所有人的平时成绩加上score再更新表
--- src/main/java/com/Service/groupService.java
+++ src/main/java/com/Service/groupService.java
@@ -1,9 +1,11 @@
 package com.Service;
 
 import com.Model.Course;
+import com.Model.Student;
 
 public interface groupService {
     void setGroupBySid(Course course, int groupNum);
     void quickAnswer(int groupId);
+    Student[] displayGroup(Course course);
     void setGroupScoreByGid(Course course,String groupId,String seminarScore);
 }
